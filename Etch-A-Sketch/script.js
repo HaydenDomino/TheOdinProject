cb6cb26d7d2d7ebf995c9a8243a0f4009f50012e
Hayden Domino <hdomino97@gmail.com>
2026-01-21 19:31:51 -0600

    Add functionality for mouse hover and reset button.

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -1,1 +1,23 @@
-const gridContainer = document.createElement("div");
+const size = 16;
+const grid = document.getElementById("grid");
+
+console.log("Creating grid");
+for (i = 0 ; i < (size * size); i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.addEventListener("mouseenter", () => {
+        square.classList.add("active");
+    });
+    grid.appendChild(square);
+}
+console.log("Grid created");
+
+const allSquares = document.querySelectorAll(".square");
+const resetButton = document.querySelector(".resetBtn");
+
+resetButton.addEventListener("click", () => {
+    allSquares.forEach(square => {
+        square.classList.remove("active");
+    });
+    console.log("Resetting now");
+});
